4739f6fc63c6c9c2da81f2b93bc20cb73e271c4e
hjs926 <joonsun926@gmail.com>
2022-12-04 19:21:39 +0900

    Update 피자 나눠먹기.js

--- 프로그래머스/피자 나눠먹기.js
+++ 프로그래머스/피자 나눠먹기.js
@@ -1,3 +1,6 @@
 function solution(n) {
   return n % 7 === 0 ? Math.floor(n / 7) : Math.floor(n / 7) + 1;
 }
+function solution(n) {
+  return n % 7 === 0 ? Math.floor(n / 7) : Math.floor(n / 7) + 1;
+}
