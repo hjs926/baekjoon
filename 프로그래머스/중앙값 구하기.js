ed04920b2cf020286da0f14a0b4312c254629937
hjs926 <joonsun926@gmail.com>
2023-01-13 20:35:22 +0900

    Update 중앙값 구하기.js

--- 프로그래머스/중앙값 구하기.js
+++ 프로그래머스/중앙값 구하기.js
@@ -1,2 +1,10 @@
 const solution = (array) =>
   array.sort((a, b) => a - b)[(array.length / 2) << 0];
+
+function solution(array) {
+  var answer = 0;
+  array = array.sort(function (a, b) {
+    return b - a;
+  });
+  return array[parseInt(array.length / 2)];
+}
