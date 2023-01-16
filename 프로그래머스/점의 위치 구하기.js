df94e5d4b060ef6fb4e224cccc83e6878f21ceb2
hjs926 <joonsun926@gmail.com>
2023-01-16 23:41:21 +0900

    Update 점의 위치 구하기.js

--- 프로그래머스/점의 위치 구하기.js
+++ 프로그래머스/점의 위치 구하기.js
@@ -14,3 +14,9 @@ function solution(dot) {
 
   return answer;
 }
+
+function solution(dot) {
+  const [num, num2] = dot;
+  const check = num * num2 > 0;
+  return num > 0 ? (check ? 1 : 4) : check ? 3 : 2;
+}
