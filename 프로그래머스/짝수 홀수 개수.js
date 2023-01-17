a98df96c0495a2cd8fd35431b992cd4acef82e26
hjs926 <joonsun926@gmail.com>
2023-01-17 23:02:43 +0900

    Update 짝수 홀수 개수.js

--- 프로그래머스/짝수 홀수 개수.js
+++ 프로그래머스/짝수 홀수 개수.js
@@ -1,5 +1,15 @@
 function solution(num_list) {
-  const evenLength = num_list.filter(n => n % 2 === 0).length;
-  
+  const evenLength = num_list.filter((n) => n % 2 === 0).length;
+
   return [evenLength, num_list.length - evenLength];
+}
+
+function solution(num_list) {
+  var answer = [0, 0];
+
+  for (let a of num_list) {
+    answer[a % 2] += 1;
+  }
+
+  return answer;
 }
