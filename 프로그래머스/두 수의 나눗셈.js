c875dbe994ab77c40b02ed0d41dbaa2167254e0b
hjs926 <joonsun926@gmail.com>
2023-02-14 19:32:24 +0900

    update

--- 프로그래머스/두 수의 나눗셈.js
+++ 프로그래머스/두 수의 나눗셈.js
@@ -3,11 +3,14 @@ function solution(num1, num2) {
   return answer;
 }
 
-// 몫을 구하는 코드
-// 소수점 뒤만 날리기 버림
-// Math.floor 사용
-
 function solution(num1, num2) {
-  var answer = Math.floor(num1 / num2);
+  var answer = Math.floor((num1 / num2) * 1000);
   return answer;
 }
+
+// 1. num1을 nm2로 나눈다.
+// 2. 나눈 값에 1000을 곱한다.
+// 3. 그 값에 정수붑ㄴ을 구한다.
+
+// parseInt와 Math.floor의 차이
+// Math.floor를 선호한다 훨씬 빠르고
--- 프로그래머스/몫 구하기.js
+++ 프로그래머스/몫 구하기.js
@@ -2,3 +2,11 @@ function solution(num1, num2) {
   var answer = (num1 / num2) >> 0;
   return answer;
 }
+// 몫을 구하는 코드
+// 소수점 뒤만 날리기 버림
+// Math.floor 사용
+
+function solution(num1, num2) {
+  var answer = Math.floor(num1 / num2);
+  return answer;
+}
