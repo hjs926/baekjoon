6801f301975a016f090f5510f0f0c2dd8e9246a5
hjs926 <joonsun926@gmail.com>
2023-02-19 21:23:41 +0900

    update

--- 프로그래머스/중앙값 구하기.js
+++ 프로그래머스/중앙값 구하기.js
@@ -8,3 +8,10 @@ function solution(array) {
   });
   return array[parseInt(array.length / 2)];
 }
+
+// arr -> 정렬 -> 가운데 숫자
+
+// 1. 배열, 제일 작은 값 찾기
+// 2. 찾으면 새 배열에 넣기
+// 3. 원래 배열에서 넣었던거 삭제
+// 1-3 반복
