1118ff961af4b420e9da638847a256b3b17ffff0
hjs926 <joonsun926@gmail.com>
2023-02-21 19:02:04 +0900

    Update 피자 나눠먹기 (1~2).js

--- 프로그래머스/피자 나눠먹기 (1~2).js
+++ 프로그래머스/피자 나눠먹기 (1~2).js
@@ -5,3 +5,25 @@ function solution(n) {
 // ceil 뭐가 나오든지 간에 올림
 
 //(2)
+// 나의 풀이
+function solution(n) {
+  var answer = 0;
+  let pizzaCount = 1;
+  while (true) {
+    if ((pizzaCount * 6) % n === 0) {
+      answer = pizzaCount;
+      break;
+    }
+    pizzaCount = pizzaCount + 1;
+  }
+  return answer;
+}
+
+// 다른사람 풀이
+function solution(n) {
+  let pizza = 1;
+  while ((pizza * 6) % n) {
+    pizza++;
+  }
+  return pizza;
+}
