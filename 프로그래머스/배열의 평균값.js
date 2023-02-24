81814079dd8492b7f77d5546e64bf9ab0fb699dd
hjs926 <joonsun926@gmail.com>
2023-02-24 16:52:15 +0900

    Update 배열의 평균값.js

--- 프로그래머스/배열의 평균값.js
+++ 프로그래머스/배열의 평균값.js
@@ -1,3 +1,4 @@
+// 나의 풀이
 function solution(numbers) {
   var answer = 0;
   let sum = 0;
@@ -6,5 +7,21 @@ function solution(numbers) {
     sum += numbers[i];
   }
   answer = sum / numbers.length;
+  return answer;
+}
+
+// 다른사람 풀이
+// (1)
+function solution(numbers) {
+  var answer = 0;
+  for (i of numbers) {
+    answer += i;
+  }
+  return answer / numbers.length;
+}
+
+// (2)
+function solution(numbers) {
+  var answer = numbers.reduce((a, b) => a + b, 0) / numbers.length;
   return answer;
 }
