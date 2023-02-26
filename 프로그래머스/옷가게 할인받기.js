bc1ce6b5ebfa6d37a4606be922c5b5ec0c8cb2f0
hjs926 <joonsun926@gmail.com>
2023-02-26 23:26:56 +0900

    Update 옷가게 할인받기.js

--- 프로그래머스/옷가게 할인받기.js
+++ 프로그래머스/옷가게 할인받기.js
@@ -28,3 +28,18 @@ const solution = (price) => {
       return Math.floor(price - (price * discount[1]) / 100);
   return price;
 };
+
+//(2)
+function solution(price) {
+  let arr_discount = [
+    [500000, 0.2],
+    [300000, 0.1],
+    [100000, 0.05],
+  ];
+
+  for (let i = 0; i < arr_discount.length; i++)
+    if (arr_discount[i][0] <= price)
+      return Math.trunc(price * (1 - arr_discount[i][1]));
+
+  return price;
+}
