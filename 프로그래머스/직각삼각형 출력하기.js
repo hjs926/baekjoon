7c412a3a6f37c171a2821ece6628514125caedd6
hjs926 <joonsun926@gmail.com>
2023-03-05 17:12:02 +0900

    Update 직각삼각형 출력하기.js

--- 프로그래머스/직각삼각형 출력하기.js
+++ 프로그래머스/직각삼각형 출력하기.js
@@ -41,3 +41,20 @@ function solution(n) {
     console.log("*".repeat(i));
   }
 }
+
+//(2)
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  for (let i = 0; i < Number(input[0]); i++) {
+    console.log("*".repeat(i + 1));
+  }
+});
