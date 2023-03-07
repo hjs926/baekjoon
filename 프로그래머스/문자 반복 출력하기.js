d275dedefd4f3a7a243fbc173b93ae6cb92ac744
hjs926 <joonsun926@gmail.com>
2023-03-07 19:05:53 +0900

    Update 문자 반복 출력하기.js

--- 프로그래머스/문자 반복 출력하기.js
+++ 프로그래머스/문자 반복 출력하기.js
@@ -1,9 +1,17 @@
 const solution = (my_string, n) =>
   [...my_string].map((a) => a.repeat(n)).join("");
 
+//다른사람의 풀이
 //1
 function solution(my_string, n) {
   var answer = [...my_string].map((v) => v.repeat(n)).join("");
   console.log(answer);
   return answer;
+}
+
+//2
+function solution(my_string, n) {
+  let ans = "";
+  for (let s of my_string) ans += s.repeat(n);
+  return ans;
 }
