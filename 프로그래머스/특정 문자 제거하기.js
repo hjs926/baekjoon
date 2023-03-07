51564315b1c399107dadb6acd22e816a031d4a5d
hjs926 <joonsun926@gmail.com>
2023-03-07 19:06:31 +0900

    Update 특정 문자 제거하기.js

--- 프로그래머스/특정 문자 제거하기.js
+++ 프로그래머스/특정 문자 제거하기.js
@@ -1,6 +1,10 @@
 function solution(my_string, letter) {
-
   const answer = my_string.split(letter).join("");
-  
+
   return answer;
+}
+
+//1
+function solution(my_string, letter) {
+  return my_string.replaceAll(letter, "");
 }
