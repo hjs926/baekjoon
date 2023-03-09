1009cf34cb9a8373693bdb4ba513d5f2f1cb9065
hjs926 <joonsun926@gmail.com>
2023-03-09 19:17:44 +0900

    Update 각도기.js

--- 프로그래머스/각도기.js
+++ 프로그래머스/각도기.js
@@ -1,16 +1,18 @@
 function solution(angle) {
-    var answer = 0;
-    if(angle<90){
-        return 1;
-    }
-    else if(angle==90){
-        return 2;
-    }
-    else if(angle<180){
-        return 3;
-    }
-    else if(angle==180){
-        return 4;
-    }
-    return answer;
+  var answer = 0;
+  if (angle < 90) {
+    return 1;
+  } else if (angle == 90) {
+    return 2;
+  } else if (angle < 180) {
+    return 3;
+  } else if (angle == 180) {
+    return 4;
+  }
+  return answer;
+}
+
+//1
+function solution(angle) {
+  return [0, 90, 91, 180].filter((x) => angle >= x).length;
 }
