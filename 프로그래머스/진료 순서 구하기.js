b64fe99a26759bd604c7756917d6f712e1035b50
hjs926 <joonsun926@gmail.com>
2023-03-17 19:59:58 +0900

    Update 진료 순서 구하기.js

--- 프로그래머스/진료 순서 구하기.js
+++ 프로그래머스/진료 순서 구하기.js
@@ -9,8 +9,17 @@ function solution(emergency) {
 }
 
 //(1)
-
 function solution(emergency) {
   let sorted = emergency.slice().sort((a, b) => b - a);
   return emergency.map((v) => sorted.indexOf(v) + 1);
+}
+
+//(2)
+function solution(emergency) {
+  let sort = [...emergency].sort((a, b) => b - a);
+  return emergency.map((k) => {
+    const queue = sort.findIndex((v) => v === k);
+
+    return queue + 1;
+  });
 }
