1640e5cb102198e166619a3e69a7d6fc1f0f83d3
hjs926 <joonsun926@gmail.com>
2023-03-27 20:09:52 +0900

    Update 가위바위보.js

--- 프로그래머스/가위바위보.js
+++ 프로그래머스/가위바위보.js
@@ -21,3 +21,19 @@ function solution(rsp) {
   const rspWin = { 2: 0, 0: 5, 5: 2 };
   return [...rsp].map((x) => rspWin[x]).join("");
 }
+
+//2
+function solution(rsp) {
+  return Array.from(rsp)
+    .map((v) => {
+      switch (+v) {
+        case 2:
+          return 0;
+        case 0:
+          return 5;
+        default:
+          return 2;
+      }
+    })
+    .join("");
+}
