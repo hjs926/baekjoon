a20ce1a09e033a17bb1ea4a4185a713fd44405aa
hjs926 <joonsun926@gmail.com>
2023-04-01 23:55:43 +0900

    Update 2차원으로 만들기.js

--- 프로그래머스/2차원으로 만들기.js
+++ 프로그래머스/2차원으로 만들기.js
@@ -24,3 +24,14 @@ function solution(num_list, n) {
   }
   return result;
 }
+
+//2
+function solution(num_list, n) {
+  const answer = [];
+
+  for (let i = 0; i < num_list.length / n; i++) {
+    answer.push(num_list.slice(i * n, i * n + n));
+  }
+
+  return answer;
+}
