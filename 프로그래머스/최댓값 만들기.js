f628683d9c3262418d7f9de92f2184ccbb535379
hjs926 <joonsun926@gmail.com>
2023-04-04 19:21:58 +0900

    Update 최댓값 만들기.js

--- 프로그래머스/최댓값 만들기.js
+++ 프로그래머스/최댓값 만들기.js
@@ -3,3 +3,9 @@ function solution(numbers) {
   const twoMaxima = numbers.splice(0, 2);
   return twoMaxima[0] * twoMaxima[1];
 }
+
+//1
+function solution(numbers) {
+  numbers.sort((a, b) => b - a);
+  return numbers[0] * numbers[1];
+}
