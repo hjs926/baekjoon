835ae156acd5110aaee0e2527321ea100a52fbe2
hjs926 <joonsun926@gmail.com>
2023-04-06 21:58:06 +0900

    Create 모음 제거.js

---
+++ 프로그래머스/모음 제거.js
@@ -0,0 +1,15 @@
+function solution(my_string) {
+  const str = "aeiou";
+
+  let newString = my_string.split("");
+
+  for (let i = 0; i < aeiou.length; i++) {
+    for (let j = 0; j < my_string.length; j++) {
+      if (newString.includes(aeiou[i])) {
+        newString.splice(newString.indexOf(aeiou[i]), 1);
+      }
+    }
+  }
+
+  return newString.join("");
+}
