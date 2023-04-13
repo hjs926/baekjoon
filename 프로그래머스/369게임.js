74fbdabed54b887be661ee74a6f8e34da09d0eb2
hjs926 <joonsun926@gmail.com>
2023-04-13 23:21:57 +0900

    Update 369게임.js

--- 프로그래머스/369게임.js
+++ 프로그래머스/369게임.js
@@ -1,2 +1,16 @@
 const solution = (order) =>
   [...(order + "")].filter((a) => ["3", "6", "9"].includes(a)).length;
+
+//1
+function solution(order) {
+  clap = [...("" + order)]; //2,9,4,2,3
+
+  count = 0;
+  for (i = 0; i < clap.length; i++) {
+    if (clap[i] % 3 === 0) {
+      count += 1;
+    }
+  }
+
+  return count;
+}
