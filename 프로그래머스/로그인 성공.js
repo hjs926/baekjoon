45d08a55028f71bf4204b875edbe26360e292adb
hjs926 <joonsun926@gmail.com>
2023-04-17 21:38:53 +0900

    Update 로그인 성공.js

--- 프로그래머스/로그인 성공.js
+++ 프로그래머스/로그인 성공.js
@@ -11,3 +11,20 @@ function solution(id_pw, db) {
 
   return result;
 }
+
+//1
+function solution(id_pw, db) {
+  let answer = "fail";
+
+  const [id, pw] = id_pw;
+  let check = false;
+
+  for (let i = 0; i < db.length; i++) {
+    if (id === db[i][0] && pw === db[i][1]) check = true;
+    if (id === db[i][0] && pw != db[i][1]) answer = "wrong pw";
+
+    if (check) answer = "login";
+  }
+
+  return answer;
+}
