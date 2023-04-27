d96e3c4f26233efeb6f7549eec73ff7e4c7930a1
hjs926 <joonsun926@gmail.com>
2023-04-27 21:42:26 +0900

    Update 7의 개수.js

--- 프로그래머스/7의 개수.js
+++ 프로그래머스/7의 개수.js
@@ -1,1 +1,10 @@
-const solution = array => array.join("").split("").filter(a => a === '7').length
+const solution = (array) =>
+  array
+    .join("")
+    .split("")
+    .filter((a) => a === "7").length;
+
+//1
+function solution(array) {
+  return array.join("").split("7").length - 1;
+}
