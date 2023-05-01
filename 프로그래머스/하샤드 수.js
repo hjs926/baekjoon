038879c83acb031622dae77912293d214094be7a
hjs926 <joonsun926@gmail.com>
2023-05-01 22:02:52 +0900

    Update 하샤드 수.js

--- 프로그래머스/하샤드 수.js
+++ 프로그래머스/하샤드 수.js
@@ -8,3 +8,17 @@ function solution(x) {
   }
   return x % sum == 0 ? true : false;
 }
+
+//1
+function solution(x) {
+  let num = x;
+  let sum = 0;
+  do {
+    sum += x % 10;
+    x = Math.floor(x / 10);
+  } while (x > 0);
+
+  return !(num % sum);
+}
+
+solution(12);
